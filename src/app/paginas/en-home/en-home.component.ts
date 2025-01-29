5e63cf9b0c4cc40f4899316709cf908bfd88686d
Pedro Jorge <skitvandarken2013@gmail.com>
2025-01-29 18:28:17 +0100

    correcção do sdwan, removido de cloud

---
+++ src/app/paginas/en-home/en-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-en-home',
+  imports: [],
+  templateUrl: './en-home.component.html',
+  styleUrl: './en-home.component.css'
+})
+export class EnHomeComponent {
+
+}
