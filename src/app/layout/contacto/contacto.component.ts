cb693a6fa4ea6d766e9161f0cc092676c7e22e23
Pedro Jorge <skitvandarken2013@gmail.com>
2025-02-07 15:33:02 +0100

    formulários funcionais, alterar o vídeo

--- src/app/layout/contacto/contacto.component.ts
+++ src/app/layout/contacto/contacto.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { TranslateService } from '@ngx-translate/core';
+import emailjs, {type EmailJSResponseStatus} from '@emailjs/browser';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contacto',
-  imports: [TranslatePipe	],
+  imports: [TranslatePipe, CommonModule, ReactiveFormsModule 	],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css'
 })
@@ -14,7 +17,47 @@ export class ContactoComponent {
           this.translate.use(language);
       }
       
-      constructor(private translate: TranslateService) {}
+      contactForm: FormGroup;
+      constructor(private translate: TranslateService, private fb: FormBuilder) {
+
+        this.contactForm = this.fb.group({
+          region: ['', Validators.required],
+          firstName: ['', Validators.required],
+          lastName: ['', Validators.required],
+          email: ['', [Validators.required, Validators.email]],
+          message: ['', Validators.required],
+          angolaCables : ['', Validators.required],
+
+        });
+
+
+
+      }
+
+      submitForm(event: Event) {
+        event.preventDefault();
+    
+        if (this.contactForm.invalid) {
+          window.alert('Por favor, preencha todos os campos obrigatórios.');
+          return;
+        }
+    
+        const formData = this.contactForm.value;
+    
+        emailjs.send('service_hxge11t', 'template_k8beyqd', formData, {
+          publicKey: '8zvRzqg96H44z9txo'
+        })
+        .then(() => {
+          console.log('SUCESSO');
+          window.alert('Mensagem enviada com sucesso! 🎉');
+          this.contactForm.reset();
+        })
+        .catch((error: EmailJSResponseStatus) => {
+          console.error('Falhou...', error.text);
+          window.alert('Não foi possível enviar sua candidatura.');
+        });
+      }
+    }   
       
 
-}
+
