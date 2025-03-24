a2e71c442700d6bf67454b675ef6d46edce7247e
Pedro Jorge <skitvandarken2013@gmail.com>
2025-03-24 12:43:20 +0100

    mvp acerca

--- src/app/paginas/acerca/acerca.component.ts
+++ src/app/paginas/acerca/acerca.component.ts
@@ -2,17 +2,24 @@ import { Component } from '@angular/core';
 import { TelcablesComponent } from '../../layout/telcables/telcables.component';
 import { MenuComponent } from '../../layout/menu/menu.component';
 import { RodapeComponent } from '../../layout/rodape/rodape.component';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-acerca',
   imports: [
 
     MenuComponent,
-    TelcablesComponent, RodapeComponent
+    TelcablesComponent, RodapeComponent, TranslatePipe
   ],
   templateUrl: './acerca.component.html',
   styleUrl: './acerca.component.css'
 })
 export class AcercaComponent {
+
+      useLanguage(language: string): void {
+            this.translate.use(language);
+        }
+        
+        constructor(private translate: TranslateService) {}
 
 }
