cb9b6da2be8e144a7486c1838db44a3ab2eee7d8
Pedro Jorge <skitvandarken2013@gmail.com>
2025-05-25 16:18:57 +0100

    eventos concluídos, painéis funcionais limitado a 2

--- src/app/layout/paineis-listar/paineis-listar.component.ts
+++ src/app/layout/paineis-listar/paineis-listar.component.ts
@@ -1,11 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { PainelService } from '../../services/painel.service';
+import { AsyncPipe, CommonModule, DatePipe, NgFor, NgIf } from '@angular/common';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { RodapeComponent } from '../rodape/rodape.component';
+import { MenuComponent } from '../menu/menu.component';
+import { TruncatePipe } from '../../pipes/truncate.pipe';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-paineis-listar',
-  imports: [],
+  imports: [
+
+        AsyncPipe,
+    CommonModule,
+    DatePipe,
+    NgFor,
+    NgIf,
+    TranslatePipe,
+    RodapeComponent,
+    TruncatePipe,
+    MenuComponent,
+    RouterLink
+
+
+  ],
   templateUrl: './paineis-listar.component.html',
   styleUrl: './paineis-listar.component.css'
 })
-export class PaineisListarComponent {
+export class PaineisListarComponent implements OnInit {
 
+
+  safeContent: SafeHtml = '';
+  private painelService = inject(PainelService);
+  paineis$ = this.painelService.getPaineis();
+
+  constructor(
+    private translate: TranslateService,
+    private sanitizer: DomSanitizer
+  ) { }
+
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
+
+  ngOnInit() {
+    this.paineis$.subscribe(paineis => {
+      if (paineis.length > 0) {
+        this.safeContent = this.sanitizer.bypassSecurityTrustHtml(paineis[0].content);
+      }
+    });
+  }
 }
--- src/app/paginas/eventos-ac/eventos-ac.component.ts
+++ src/app/paginas/eventos-ac/eventos-ac.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { MenuComponent } from '../../layout/menu/menu.component';
 import { RodapeComponent } from '../../layout/rodape/rodape.component';
 import { EventosListarComponent } from '../../layout/eventos-listar/eventos-listar.component';
+import { PaineisListarComponent } from "../../layout/paineis-listar/paineis-listar.component";
 
 @Component({
   selector: 'app-eventos-ac',
   imports: [
     MenuComponent,
     RodapeComponent,
-    EventosListarComponent
-  ],
+    EventosListarComponent,
+    PaineisListarComponent
+],
   templateUrl: './eventos-ac.component.html',
   styleUrl: './eventos-ac.component.css'
 })
