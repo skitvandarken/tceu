fb56f4a4e49b3e4af8089070d509669b9d1b6d13
Pedro Jorge <skitvandarken2013@gmail.com>
2025-07-04 12:19:53 +0100

    adslider, repositioning

--- src/app/paginas/contacto/contacto.component.ts
+++ src/app/paginas/contacto/contacto.component.ts
@@ -18,94 +18,99 @@ interface Address {
 })
 export class ContactoComponent {
 
-         useLanguage(language: string): void {
-              this.translate.use(language);
-          }
-          
-          constructor(private translate: TranslateService) {}
-          selectedCountry: string = 'Angola';
-  
-          countries = [
-            { name: 'Angola', value: '0' },
-            { name: 'África do Sul', value: '1' },
-            { name: 'Nigéria', value: '2' },
-            { name: 'Europa', value: '3' },
-            { name: 'Brasil', value: '4' },
-            { name: 'EUA', value: '5' },
-          ];
-        
-          addresses: Record<string, Address[]> = {
-            'Angola': [
-              {
-                title: 'Sede',
-                address: 'Edifício Cellwave, Via AL5 Zona, XR6B<br>Talatona, Luanda Sul, Angola',
-              },
-              {
-                title: 'Data Center - AngoNAP Luanda',
-                address: 'Avenida Pedro Castro Van Dúnem Loy<br>Talatona, Luanda Sul, Angola',
-              },
-              {
-                title: 'Estação Sangano',
-                address: 'Praia de Sangano, Município da Kissama, <br> Bengo, Angola',
-              }
-            ],
-            'África do Sul': [
-              {
-                title: 'Johannesburg',
-                address: 'Suite 1, The Pivot, Block E <br> 1 Montecasino Boulevard, Fourways<br> 2191 - Gauteng, South Africa',
-               
-              },
-        
-            ],
-            'Nigéria': [
-              {
-                title: 'Lagos Headquarters',
-                address: '5 Jeremiah Ugwu Street, Off Babatunde Anjous<br>Lekki Phase 1,<br>Lagos, Nigeria',
-              }
-            ],
-            'Portugal': [
-              {
-                title: 'Lisboa',
-                address: 'Avenida da Liberdade, 245, 5º <br> 1250-143 Lisboa, Portugal',
-              }
-            ],
-            'Brasil': [
-              {
-                title: 'São Paulo',
-                address: 'Rua Irmã Gabriela, 51 <br>Brooklin <br>CEP 04571-130 – São Paulo',
-              },
-              {
-                title: 'Data Center  AngonaNAP Fortaleza',
-                address: 'Avenida Trajano de Medeiros, 1851 <br> Praia do Futuro I – Fortaleza – Ce <br>CEP 04571-130 – São Paulo <br> CEP 60.182-325',
-              }
-            ],
-            'EUA': [
-              {
-                title: 'Bermuda',
-                address: 'Canon’s Court,22 Victoria Street, <br> HM 12, Bermuda',
-              },
-            
-            ],
-            'Europa': [
-              {
-                title: 'Escritório - Portugal',
-                address: 'Rua da Alfândega, n.º 64, 5.º andar,<br> 9000 059 FUNCHAL <br> Madeira – Portugal',               
-              },
-         
-            ]
-          };
-        
-          onCountryChange(event: Event) {
-            const select = event.target as HTMLSelectElement;
-            this.selectedCountry = select.value;
-          }
-        
-          getCountryAddresses(country: string): Address[] {
-            return this.addresses[country] || [{
-              title: 'Contact Support',
-              address: 'Please contact us for address information in this country',
-              phone: '+244 927 686 206'
-            }];
-          }
+  useLanguage(language: string): void {
+    this.translate.use(language);
+  }
+
+  constructor(private translate: TranslateService) { }
+  selectedCountry: string = 'Angola';
+
+  countries = [
+    { name: 'Angola', value: '0' },
+    { name: 'África do Sul', value: '1' },
+    { name: 'Nigéria', value: '2' },
+    { name: 'Europa', value: '3' },
+    { name: 'Brasil', value: '4' },
+    { name: 'EUA', value: '5' },
+  ];
+
+  addresses: Record<string, Address[]> = {
+    'Angola': [
+      {
+        title: 'Sede',
+        address: 'Edifício Cellwave, Via AL5 Zona, XR6B<br>Talatona, Luanda Sul, Angola',
+        phone: '+244 227 360 006'
+      },
+      {
+        title: 'Data Center - AngoNAP Luanda',
+        address: 'Avenida Pedro Castro Van Dúnem Loy<br>Talatona, Luanda Sul, Angola',
+      },
+      {
+        title: 'Estação Sangano',
+        address: 'Praia de Sangano, Município da Kissama, <br> Bengo, Angola',
+      }
+    ],
+    'África do Sul': [
+      {
+        title: 'Johannesburg',
+        address: 'Suite 1, The Pivot, Block E <br> 1 Montecasino Boulevard, Fourways<br> 2191 - Gauteng, South Africa',
+        phone: '+27 (0) 10 005 5865'
+
+      },
+
+    ],
+    'Nigéria': [
+      {
+        title: 'Lagos Headquarters',
+        address: '5 Jeremiah Ugwu Street, Off Babatunde Anjous<br>Lekki Phase 1,<br>Lagos, Nigeria',
+      }
+    ],
+    'Portugal': [
+      {
+        title: 'Lisboa',
+        address: 'Avenida da Liberdade, 245, 5º <br> 1250-143 Lisboa, Portugal',
+      }
+    ],
+    'Brasil': [
+      {
+        title: 'São Paulo',
+        address: 'Rua Irmã Gabriela, 51 <br>Brooklin <br>CEP 04571-130 – São Paulo',
+
+      },
+      {
+        title: 'Data Center  AngonaNAP Fortaleza',
+        address: 'Avenida Trajano de Medeiros, 1851 <br> Praia do Futuro I – Fortaleza – Ce <br>CEP 04571-130 – São Paulo <br> CEP 60.182-325',
+        phone: '+55 85 99645-4143'
+
+      }
+    ],
+    'EUA': [
+      {
+        title: 'Bermuda',
+        address: 'Canon’s Court,22 Victoria Street, <br> HM 12, Bermuda',
+      },
+
+    ],
+    'Europa': [
+      {
+        title: 'Escritório - Portugal',
+        address: 'Rua da Alfândega, n.º 64, 5.º andar,<br> 9000 059 FUNCHAL <br> Madeira – Portugal',
+      },
+
+    ]
+  };
+
+  onCountryChange(event: Event) {
+    const select = event.target as HTMLSelectElement;
+    this.selectedCountry = select.value;
+  }
+
+  getCountryAddresses(country: string): Address[] {
+    return this.addresses[country] || [{
+      title: 'Contact Support',
+      address: 'Please contact us for address information in this country',
+      phone: '+244 927 686 206'
+    }];
+  }
 
 }
