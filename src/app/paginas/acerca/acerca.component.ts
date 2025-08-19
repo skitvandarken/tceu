52f2b9619b5a88e5458466528cf88d30759c3bfd
Pedro Jorge <skitvandarken2013@gmail.com>
2025-08-19 15:11:20 +0100

    company

--- src/app/paginas/acerca/acerca.component.ts
+++ src/app/paginas/acerca/acerca.component.ts
@@ -4,19 +4,25 @@ import { MenuComponent } from '../../layout/menu/menu.component';
 import { RodapeComponent } from '../../layout/rodape/rodape.component';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { Ad2Component } from "../../layout/ad2/ad2.component";
+import { SlideshowComponent } from '../../layout/slideshow/slideshow.component';
+import { NumerosComponent } from '../../layout/numeros/numeros.component';
+import { ContadorComponent } from '../../layout/contador/contador.component';
+import { NoticiasListarComponent } from '../../layout/noticias-listar/noticias-listar.component';
+import { EventosListarComponent } from '../../layout/eventos-listar/eventos-listar.component';
+import { BlogComponent } from "../../layout/blog/blog.component";
 
 @Component({
   selector: 'app-acerca',
   imports: [
     MenuComponent,
-    TelcablesComponent, RodapeComponent, TranslatePipe,
-    Ad2Component
+    TelcablesComponent, RodapeComponent, TranslatePipe, NoticiasListarComponent, EventosListarComponent,
+    Ad2Component, SlideshowComponent, NumerosComponent, ContadorComponent,
+    BlogComponent
 ],
   templateUrl: './acerca.component.html',
   styleUrl: './acerca.component.css'
 })
 export class AcercaComponent {
-
 
 
       useLanguage(language: string): void {
